e09ee34e84918497a6390e5abd9ae8a65ed54a14
jpcchaves <joaopaulodecastrochaves@hotmail.com>
2023-02-09 16:48:30 -0300

    feat: add edit category endpoint

--- src/main/java/com/ds/dscatalog/services/CategoryService.java
+++ src/main/java/com/ds/dscatalog/services/CategoryService.java
@@ -3,11 +3,12 @@ package com.ds.dscatalog.services;
 import com.ds.dscatalog.dto.CategoryDTO;
 import com.ds.dscatalog.entities.Category;
 import com.ds.dscatalog.repositories.CategoryRepository;
-import com.ds.dscatalog.services.exceptions.EntityNotFoundException;
+import com.ds.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -30,17 +31,34 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Não foi encontrado uma categoria com o ID informado: " + id));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Não foi encontrado uma categoria com o ID informado: " + id));
 
         return new CategoryDTO(entity);
     }
 
     @Transactional
     public CategoryDTO insert(CategoryDTO dto) {
+
         Category entity = new Category();
         entity.setName(dto.getName());
         entity = repository.save(entity);
-
         return new CategoryDTO(entity);
+
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+
+            Category entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+
+            return new CategoryDTO(entity);
+
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Não foi possível atualizar a categoria pois o ID informado não existe: " + dto.getId());
+
+        }
     }
 }
--- src/main/java/com/ds/dscatalog/services/exceptions/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ds.dscatalog.services.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
