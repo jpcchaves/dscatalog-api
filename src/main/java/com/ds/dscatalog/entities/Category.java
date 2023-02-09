6ddc2cfe53d323f2820bace5f97b7fa7dd227a9f
jpcchaves <joaopaulodecastrochaves@hotmail.com>
2023-02-09 13:07:20 -0300

    feat: add category entity

---
+++ src/main/java/com/ds/dscatalog/entities/Category.java
@@ -0,0 +1,51 @@
+package com.ds.dscatalog.entities;
+
+import java.util.Objects;
+
+public class Category {
+
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        if (!id.equals(category.id)) return false;
+        return Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
