ea8b48e4b45e358a8eadfc06149a524dc4c5dbd5
jpcchaves <joaopaulodecastrochaves@hotmail.com>
2023-02-09 16:59:59 -0300

    feat: add delete category endpoint

--- src/main/java/com/ds/dscatalog/services/CategoryService.java
+++ src/main/java/com/ds/dscatalog/services/CategoryService.java
@@ -3,8 +3,11 @@ package com.ds.dscatalog.services;
 import com.ds.dscatalog.dto.CategoryDTO;
 import com.ds.dscatalog.entities.Category;
 import com.ds.dscatalog.repositories.CategoryRepository;
+import com.ds.dscatalog.services.exceptions.DatabaseException;
 import com.ds.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -59,6 +62,16 @@ public class CategoryService {
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Não foi possível atualizar a categoria pois o ID informado não existe: " + dto.getId());
 
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Não foi possível excluir a categoria pois o ID informado não existe: " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Não foi possível deletar a categoria pois acarretaria em uma inconsistência na integridade do banco de dados");
         }
     }
 }
